3c7a17c3e87b364737bc4ea26f0d45afe68b36ac
Shashaankar <shashaankar61@gmail.com>
2016-09-05 21:27:15 -0700

    Template code added for trove commands

--- src/reactive/trove_utils.py
+++ src/reactive/trove_utils.py
@@ -137,30 +137,128 @@ def display_servers():
     for server in get_servers():
         display(server)
 
+def trove_create(**cmd_args):
+    for k,v in cmd_args.iteritems():
+        print "%s = %s" % (k, v)
+    # TODO
+    pass
+
+def trove_db_create(**cmd_args):
+    # TODO
+    pass
+
+def trove_db_delete(**cmd_args):
+    # TODO
+    pass
+
+def trove_db_list(**cmd_args):
+    # TODO
+    pass
+
+def trove_delete(**cmd_args):
+    # TODO
+    pass
+
+def trove_list(**cmd_args):
+    # TODO
+    pass
+
+def trove_show(**cmd_args):
+    # TODO
+    pass
 
 if __name__ == '__main__':
     commands = {
-        'domain-create': create_domain,
-        'server-create': create_server,
-        'domain-get': display_domain_id,
-        'server-get': display_server_id,
-        'domain-delete': delete_domain,
-        'domain-list': display_domains,
-        'server-list': display_servers,
+    #    'domain-create': create_domain,
+    #    'server-create': create_server,
+    #    'domain-get': display_domain_id,
+    #    'server-get': display_server_id,
+    #    'domain-delete': delete_domain,
+    #    'domain-list': display_domains,
+    #    'server-list': display_servers,
+        'trove-create': trove_create,
+        'trove-database-create': trove_db_create,
+        'trove-database-delete': trove_db_delete,
+        'trove-database-list': trove_db_list,
+        'trove-delete': trove_delete,
+        'trove-list': trove_list,
+        'trove-show': trove_show,
     }
-    parser = argparse.ArgumentParser(description='Manage designate.')
+    parser = argparse.ArgumentParser(description='trove cli reference')
     parser.add_argument('command',
                         help='One of: {}'.format(', '.join(commands.keys())))
-    parser.add_argument('--domain-name', help='Domain Name')
-    parser.add_argument('--server-name', help='Server Name')
-    parser.add_argument('--email', help='Email Address')
+    #parser.add_argument('--domain-name', help='Domain Name')
+    #parser.add_argument('--server-name', help='Server Name')
+    #parser.add_argument('--email', help='Email Address')
+
+    # must arguments for trove create
+    parser.add_argument('--name', help='Name of the instance(trove-create) or database (trove-database-create)')
+    parser.add_argument('--flavor', help='A flavor name or ID')
+    # must arguments for trove db create
+    parser.add_argument('--instance', help='ID or name of the instance')
+    # must arg for trove db delete
+    parser.add_argument('--database', help='Name of the database.')
+
+    # optional args for trove db create
+    parser.add_argument('--character-set', action='store_true', help='Optional character set for database.')
+    parser.add_argument('--collate', action='store_true', help='Optional collation type for database.')
+
+    # optional arguments for trove create
+    parser.add_argument('--size', default=0, action='store_true', help='Size of the instance disk volume in GB. Required when volume support is enabled.')
+    parser.add_argument('--volume_type', action='store_true', help='Volume type. Optional when volume support is enabled.')
+    parser.add_argument('--databases', action='store_true', help='Optional list of databases..')
+    parser.add_argument('--users', action='store_true', help='Optional list of users: <user:password> [<user:password> ...]')
+    parser.add_argument('--backup', action='store_true', help='A backup name or ID.')
+    parser.add_argument('--availability_zone', action='store_true', help='The Zone hint to give to Nova: --availability_zone <availability_zone>')
+    parser.add_argument('--datastore', action='store_true', help='The datastore name or ID: --datastore <datastore>')
+    parser.add_argument('--datastore_version', action='store_true', help='The datastore version name or ID: --datastore_version <datastore_version>')
+    parser.add_argument('--configuration', action='store_true', help='ID of the configuration group to attach to the instance.')
+    parser.add_argument('--replica_of', action='store_true', help='ID or name of an existing instance to replicate from.')
+    parser.add_argument('--replica_count', default=1, action='store_true', help='Number of replicas to create (defaults to 1 if replica_of specified).')
+    parser.add_argument('--module', action='store_true', help='ID or name of the module to apply. Specify multiple times to apply multiple modules.')
+    parser.add_argument('--locality', action='store_true', help='Locality policy to use when creating replicas. Choose one of affinity, anti-affinity.')
+
+    # optional args for trove list
+    parser.add_argument('--limit', action='store_true', help='Limit the number of results displayed')
+    parser.add_argument('--marker', action='store_true', help='Begin displaying the results for IDs greater than the specified marker. When used with --limit, set this to last ID displayed in the previous run')
+
     args = parser.parse_args()
-    # extract domain_name, server_name, email if they exist into a dictionary
+    # extract essential args if they exist into a dictionary
     # to pass to the command -- this enforces that the right thing is passed to
     # a function that is expecting an argument.
     cmd_args = {v: a
                 for v, a in zip(
-                        ('domain_name', 'server_name', 'domain_email'),
-                        (args.domain_name, args.server_name, args.email))
+                        ('name', 'flavor', 'instance', 'database'),
+                        (args.name, args.flavor, args.instance, args.database))
                 if a}
+    # check for command specific required args
+    if args.command == 'trove-create':
+        if (args.name == None or args.flavor == None):
+            raise RuntimeError("Error: {} required args --name --flavor".format(args.command))
+
+    if args.command == 'trove-database-create':
+        if args.instance == None or args.name == None:
+            raise RuntimeError("Error: {} required args --instance --name".format(args.command))
+            exit(-1)
+
+    if args.command == 'trove-database-delete':
+        if args.instance == None or args.database == None:
+            raise RuntimeError("Error: {} required args --instance --database".format(args.command))
+            exit(-1)
+
+    if args.command == 'trove-database-list':
+        if args.instance == None:
+            raise RuntimeError("Error: {} required args --instance".format(args.command))
+            exit(-1)
+
+    if args.command == 'trove-delete':
+        if args.instance == None:
+            raise RuntimeError("Error: {} required args --instance".format(args.command))
+            exit(-1)
+
+    if args.command == 'trove-show':
+        if args.instance == None:
+            raise RuntimeError("Error: {} required args --instance".format(args.command))
+            exit(-1)
+
     commands[args.command](**cmd_args)
