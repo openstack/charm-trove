8966e15476f22b7a2e235d40f03885aff81179cd
Jonathan Arrance <jarrance@netdevgroup.com>
2016-09-07 00:22:37 -0400

    Built a new Trove.py based on the barbican charm. It more closely relates to what we are doing.

--- src/lib/charm/openstack/trove.py
+++ src/lib/charm/openstack/trove.py
@@ -11,240 +11,188 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# The trove handlers class
 
-import contextlib
-import os
+# bare functions are provided to the reactive handlers to perform the functions
+# needed on the class.
+from __future__ import absolute_import
+
 import subprocess
-import uuid
 
 import charmhelpers.contrib.openstack.utils as ch_utils
-import openstack_adapters as openstack_adapters
-import charms_openstack.charm as openstack_charm
-import charms_openstack.ip as os_ip
-import charmhelpers.core.decorators as decorators
 import charmhelpers.core.hookenv as hookenv
-import charmhelpers.core.host as host
+import charmhelpers.core.unitdata as unitdata
+import charmhelpers.fetch
 
-TROVE_DIR = '/etc/trove'
-TROVE_DEFAULT = '/etc/default/openstack'
-TROVE_CONF = TROVE_DIR + '/trove.conf'
-#RC_FILE = '/root/novarc'
+import charms_openstack.charm
+import charms_openstack.adapters
+import charms_openstack.ip as os_ip
+
+PACKAGES = ['python-trove', 'python-troveclient', 'trove-common', 'trove-api', 'trove-taskmanager','trove-conductor']
+
+TROVE_DIR = '/etc/trove/'
+TROVE_CONF = TROVE_DIR + "trove.conf"
+TROVE_API_PASTE_CONF = TROVE_DIR + "api-paste.ini"
+TROVE_CONDUCTOR = TROVE_DIR + "trove-conductor.conf"
+TROVE_GUEST_AGENT = TROVE_DIR + "trove-guestagent.conf"
+TROVE_TASK_MANAGER = TROVE_DIR + "trove-taskmanager.conf"
+
+OPENSTACK_RELEASE_KEY = 'trove-charm.openstack-release-version'
 
 
-# select the default release function to choose the right Charm class
-openstack_charm.use_defaults('charm.default-select-release')
+###
+# Handler functions for events that are interesting to the Trove charms
 
-'''
-def render_sink_configs(interfaces_list):
-    """Use the singleton from the TroveCharm to render sink configs
-
-    @param interfaces_list: List of instances of interface classes.
-    @returns: None
+def install():
+    """Use the singleton from the TroveCharm to install the packages on the
+    unit
     """
-    configs = [TROVE_DEFAULT]
-    TroveCharm.singleton.render_with_interfaces(
-        interfaces_list,
-        configs=configs)
-'''
+    unitdata.kv().unset(OPENSTACK_RELEASE_KEY)
+    TroveCharm.singleton.install()
 
-# Get database URIs for the two trove databases
-@openstack_adapters.adapter_property('shared-db')
-def trove_uri(db):
-    """URI for trove DB"""
-    return db.get_uri(prefix='trove')
 
-class TroveCharm(openstack_charm.HAOpenStackCharm):
+def setup_endpoint(keystone):
+    """When the keystone interface connects, register this unit in the keystone
+    catalogue.
 
+    :param keystone: instance of KeystoneRequires() class from i/f
+    """
+    charm = TroveCharm.singleton
+    keystone.register_endpoints(charm.service_type,
+                                charm.region,
+                                charm.public_url,
+                                charm.internal_url,
+                                charm.admin_url)
+
+
+def render_configs(interfaces_list):
+    """Using a list of interfaces, render the configs and, if they have
+    changes, restart the services on the unit.
+
+    :param interfaces_list: [RelationBase] interfaces from reactive
+    """
+    TroveCharm.singleton.render_with_interfaces(interfaces_list)
+
+def assess_status():
+    """Just call the TroveCharm.singleton.assess_status() command to update
+    status on the unit.
+    """
+    TroveCharm.singleton.assess_status()
+
+
+def configure_ssl(keystone=None):
+    """Use the singleton from the TroveCharm to configure ssl
+
+    :param keystone: KeystoneRequires() interface class
+    """
+    TroveCharm.singleton.configure_ssl(keystone)
+
+
+###
+# Implementation of the Trove Charm classes
+
+class TroveConfigurationAdapter(
+        charms_openstack.adapters.APIConfigurationAdapter):
+
+    def __init__(self, port_map=None):
+        super(TroveConfigurationAdapter, self).__init__(
+            service_name='trove',
+            port_map=port_map)
+        if self.keystone_api_version not in ['2', '3', 'none']:
+            raise ValueError(
+                "Unsupported keystone-api-version ({}). It should be 2 or 3"
+                .format(self.keystone_api_version))
+
+    @property
+    def trove_api_keystone_pipeline(self):
+        if self.keystone_api_version == "2":
+            return 'cors keystone_authtoken context apiapp'
+        else:
+            return 'cors keystone_v3_authtoken context apiapp'
+
+    @property
+    def trove_api_pipeline(self):
+        return {
+            "2": "cors keystone_authtoken context apiapp",
+            "3": "cors keystone_v3_authtoken context apiapp",
+            "none": "cors unauthenticated-context apiapp"
+        }[self.keystone_api_version]
+
+    @property
+    def trove_api_keystone_audit_pipeline(self):
+        if self.keystone_api_version == "2":
+            return 'keystone_authtoken context audit apiapp'
+        else:
+            return 'keystone_v3_authtoken context audit apiapp'
+
+
+class TroveAdapters(charms_openstack.adapters.OpenStackAPIRelationAdapters):
+    """
+    Adapters class for the Trove charm.
+
+    This plumbs in the TroveConfigurationAdapter as the ConfigurationAdapter
+    to provide additional properties.
+    """
+
+    def __init__(self, relations):
+        super(TroveAdapters, self).__init__(
+            relations,
+            options_instance=TroveConfigurationAdapter(
+                port_map=TroveCharm.api_ports))
+
+
+class TroveCharm(charms_openstack.charm.HAOpenStackCharm):
+    """TroveCharm provides the specialisation of the OpenStackCharm
+    functionality to manage a trove unit.
+    """
+
+    release = 'mitaka'
     name = 'trove'
-    packages = [ 'build-essential', 'libxslt1-dev', 'qemu-utils',
-                'mysql-client', 'git', 'python-dev', 'python-pexpect',
-                'python-pymysql', 'libmysqlclient-dev', 'python-apt',
-                'python-trove', 'python-troveclient', 'python-glanceclient',
-                'trove-common', 'trove-api', 'trove-taskmanager','trove-conductor']
-
-    services = ['trove-api', 'trove-taskmanager','trove-conductor']
-
+    packages = PACKAGES
     api_ports = {
-        'trove-api': {
+        'trove-worker': {
             os_ip.PUBLIC: 8779,
             os_ip.ADMIN: 8779,
             os_ip.INTERNAL: 8779,
         }
     }
+    service_type = 'trove'
+    default_service = 'trove-worker'
+    services = ['trove-api', 'trove-taskmanager', 'trove-conductor']
 
-    required_relations = ['shared-db', 'amqp', 'identity-service', 'image-service', 'cloud-compute', ]
+    # Note that the hsm interface is optional - defined in config.yaml
+    required_relations = ['shared-db', 'amqp', 'identity-service', 'image-service', 'cloud-compute',cinder heat swift]
 
     restart_map = {
-        '/etc/default/openstack': services,
-        '/etc/trove/trove.conf': services,
-        '/etc/trove/trove-conductor.conf': services,
-        '/etc/trove/trove-taskmanager.conf': services,
-        RC_FILE: [''],
+        TROVE_CONF: services,
+        TROVE_API_PASTE_CONF: services,
+        TROVE_CONDUCTOR: services,
+        TROVE_TASK_MANAGER: services
     }
-    service_type = 'trove'
-    default_service = 'trove-api'
-    sync_cmd = ['trove-manage', 'db_sync']
 
+    adapters_class = TroveAdapters
     ha_resources = ['vips', 'haproxy']
-    release = 'mitaka'
 
-    def get_amqp_credentials(self):
-        """Provide the default amqp username and vhost as a tuple.
-
-        :returns (username, host): two strings to send to the amqp provider.
+    def install(self):
+        """Customise the installation, configure the source and then call the
+        parent install() method to install the packages
         """
-        return ('trove', 'openstack')
+        self.configure_source()
+        # and do the actual install
+        super(TroveCharm, self).install()
 
-    def get_database_setup(self):
-        """Provide the default database credentials as a list of 3-tuples
 
-        returns a structure of:
-        [
-            {'database': <database>,
-             'username': <username>,
-             'hostname': <hostname of this unit>
-             'prefix': <the optional prefix for the database>, },
-        ]
+# Determine the charm class by the supported release
+@charms_openstack.charm.register_os_release_selector
+def select_release():
+    """Determine the release based on the python-keystonemiddleware that is
+    installed.
 
-        :returns [{'database': ...}, ...]: credentials for multiple databases
-        """
-        ip = hookenv.unit_private_ip()
-        return [
-            dict(
-                database='trove',
-                username='trove',
-                hostname=ip,
-                prefix='trove'),
-        ]
-
-    def render_base_config(self, interfaces_list):
-        """Render initial config to bootstrap Trove service
-
-        @returns None
-        """
-        configs = [RC_FILE, TROVE_CONF, TROVE_DEFAULT]
-        if self.haproxy_enabled():
-            configs.append(self.HAPROXY_CONF)
-        self.render_with_interfaces(
-            interfaces_list,
-            configs=configs)
-
-    def render_full_config(self, interfaces_list):
-        """Render all config for Trove service
-
-        @returns None
-        """
-        # Render base config first to ensure Trove API is responding as
-        # sink configs rely on it.
-        self.render_base_config(interfaces_list)
-        self.render_with_interfaces(interfaces_list)
-    '''
-    @classmethod
-    def create_domain(cls, domain, email):
-        """Create a domain
-
-        @param domain: The name of the domain you are creating. The name must
-                       end with a full stop.
-        @param email: An email address of the person responsible for the
-                      domain.
-        @returns None
-        """
-        cls.ensure_api_responding()
-        create_cmd = ['reactive/trove_utils.py', 'domain-create',
-                      '--domain-name', domain, '--email', email]
-        subprocess.check_call(create_cmd)
-    '''
-
-    @classmethod
-    def create_server(cls, nsname):
-        """ create a nameserver entry with the supplied name
-
-        @param nsname: Name of NameserverS record
-        @returns None
-        """
-        cls.ensure_api_responding()
-        create_cmd = ['reactive/trove_utils.py', 'server-create',
-                      '--server-name', nsname]
-        subprocess.check_call(create_cmd)
-
-    @classmethod
-    def domain_init_done(cls):
-        """Query leader db to see if domain creation is donei
-
-        @returns boolean"""
-        return hookenv.leader_get(attribute='domain-init-done')
-
-    @classmethod
-    @decorators.retry_on_exception(
-        10, base_delay=5, exc_type=subprocess.CalledProcessError)
-    def ensure_api_responding(cls):
-        """Check that the api service is responding.
-
-        The retry_on_exception decorator will cause this method to be called
-        until it succeeds or retry limit is exceeded"""
-        hookenv.log('Checking API service is responding',
-                    level=hookenv.WARNING)
-        check_cmd = ['reactive/trove_utils.py', 'server-list']
-        subprocess.check_call(check_cmd)
-
-    @classmethod
-    @contextlib.contextmanager
-    def check_zone_ids(cls, nova_domain_name, neutron_domain_name):
-        zone_org_ids = {
-            'nova-domain-id': cls.get_domain_id(nova_domain_name),
-            'neutron-domain-id': cls.get_domain_id(neutron_domain_name),
-        }
-        yield
-        zone_ids = {
-            'nova-domain-id': cls.get_domain_id(nova_domain_name),
-            'neutron-domain-id': cls.get_domain_id(neutron_domain_name),
-        }
-        if zone_org_ids != zone_ids:
-            # Update leader-db to trigger peers to rerender configs
-            # as sink files will need updating with new domain ids
-            # Use host ID and current time UUID to help with debugging
-            hookenv.leader_set({'domain-init-done': uuid.uuid1()})
-
-    @classmethod
-    def create_initial_servers_and_domains(cls):
-        """Create the nameserver entry and domains based on the charm user
-        supplied config
-
-        @returns None
-        """
-        if hookenv.is_leader():
-            cls.ensure_api_responding()
-            nova_domain_name = hookenv.config('nova-domain')
-            neutron_domain_name = hookenv.config('neutron-domain')
-            with cls.check_zone_ids(nova_domain_name, neutron_domain_name):
-                if hookenv.config('nameservers'):
-                    for ns in hookenv.config('nameservers').split():
-                        cls.create_server(ns)
-                else:
-                    hookenv.log('No nameserver specified, skipping creation of'
-                                'nova and neutron domains',
-                                level=hookenv.WARNING)
-                    return
-                if nova_domain_name:
-                    cls.create_domain(
-                        nova_domain_name,
-                        hookenv.config('nova-domain-email'))
-                if neutron_domain_name:
-                    cls.create_domain(
-                        neutron_domain_name,
-                        hookenv.config('neutron-domain-email'))
-
-    def update_pools(self):
-        # designate-manage communicates with designate via message bus so no
-        # need to set OS_ vars
-        if hookenv.is_leader():
-            cmd = ['trove-manage', 'pool', 'update']
-            subprocess.check_call(cmd)
-
-    def custom_assess_status_check(self):
-        if (not hookenv.config('nameservers') and
-                (hookenv.config('nova-domain') or
-                 hookenv.config('neutron-domain'))):
-            return 'blocked', ('nameservers must be set when specifying'
-                               ' nova-domain or neutron-domain')
-        return None, None
+    Note that this function caches the release after the first install so that
+    it doesn't need to keep going and getting it from the package information.
+    """
+    release_version = unitdata.kv().get(OPENSTACK_RELEASE_KEY, None)
+    if release_version is None:
+        release_version = ch_utils.os_release('python-keystonemiddleware')
+        unitdata.kv().set(OPENSTACK_RELEASE_KEY, release_version)
+    return release_version
